45d75727279ecf8f626fec5fb87cce4e519ce472
hieunguyen26 <nguyencaohieu26@gmail.com>
2021-06-27 21:42:17 +0700

    FAQ html2

--- assets/js/faq.js
+++ assets/js/faq.js
@@ -1,7 +1,7 @@
 const btn_OpenFAQ = document.querySelectorAll(".faq__title");
 const contentFAQ  = document.querySelectorAll(".faq__sub-content");
 const faq__maincontent = document.querySelectorAll(".faq__main-content-item");
-btn_OpenFAQ.forEach(function(item,index){
+btn_OpenFAQ.forEach(function(item){
   item.addEventListener("click",function(e){
     const contentChose = e.currentTarget.parentElement;
     const parent = e.currentTarget;
@@ -12,5 +12,22 @@ btn_OpenFAQ.forEach(function(item,index){
       }
       item.classList.remove("active");
     })
+  })
+})
+// 
+const btn_ChoseFAQPart = document.querySelectorAll(".faq__main-btn-chose");
+const faq_listFAQ      = document.querySelectorAll(".faq__main-details-item");
+btn_ChoseFAQPart.forEach(function(item,index){
+  item.addEventListener("click",function(e){
+    e.preventDefault()
+    var nameFAQ = e.currentTarget.innerText;
+    faq_listFAQ.forEach(function(item){
+      if(nameFAQ == item.getAttribute("data-faq")){
+        item.classList.add("active");
+        return;
+      }
+      item.classList.remove("active");
+    })
+    choseactive(index,btn_ChoseFAQPart)
   })
 })
